3ef516342e465ab602722cedc6d04c8b7914e9f5
Xiong Changnian <xiong@cpan.org>
2013-02-12 20:29:55 -0800

    Paste boilerplate POD and subst manually.
    Signed-off-by: Xiong Changnian <xiong@cpan.org>

--- lib/Devel/Toolbox.pm
+++ lib/Devel/Toolbox.pm
@@ -25,3 +25,126 @@ use warnings;
 #============================================================================#
 __END__
 
+=head1 NAME
+
+Devel::Toolbox - Simple custom project tool management
+
+=head1 VERSION
+
+This document describes Devel::Toolbox version v0.0.0
+
+=head1 SYNOPSIS
+
+    $ dt setup
+    $ dt help
+    $ dt list sets
+    $ dt list tools SomeSet
+    $ dt new My::Dist
+    $ dt new test fubar
+    $ dt build
+    $ dt release
+
+=head1 DESCRIPTION
+
+=over
+
+I<The computer should be doing the hard work. 
+That's what it's paid to do, after all.> 
+-- Larry Wall
+
+=back
+
+=head1 METHODS 
+
+=head2 new()
+
+=head1 ACCSESSORS
+
+Object-oriented accessor methods are provided for each parameter and result. 
+They all do just what you'd expect. 
+
+    $self               = $self->put_attr($string);
+    $string             = $self->get_attr();
+
+=head1 SEE ALSO
+
+L<Some::Module|Some::Module>
+
+=head1 INSTALLATION
+
+This module is installed using L<Module::Build|Module::Build>. 
+
+=head1 DIAGNOSTICS
+
+=over
+
+=item C<< some error message >>
+
+Some explanation. 
+
+=back
+
+=head1 CONFIGURATION AND ENVIRONMENT
+
+Devel::Toolbox requires no configuration files or environment variables.
+
+=head1 DEPENDENCIES
+
+There are no non-core dependencies. 
+
+=over
+
+=item 
+
+L<version|version> 0.99    
+    E<nbsp>E<nbsp>E<nbsp>E<nbsp> # Perl extension for Version Objects
+
+=back
+
+This module should work with any version of perl 5.16.2 and up. 
+However, you may need to upgrade some core modules. 
+
+=head1 INCOMPATIBILITIES
+
+None known.
+
+=head1 BUGS AND LIMITATIONS
+
+This is an early release. Reports and suggestions will be warmly welcomed. 
+
+Please report any bugs or feature requests, or other issues through 
+the web interface at
+L<https://github.com/xiong/devel-toolbox/issues>.
+
+=head1 DEVELOPMENT
+
+This project is hosted on GitHub at: 
+L<https://github.com/xiong/devel-toolbox>. 
+
+=head1 THANKS
+
+Somebody helped!
+
+=head1 AUTHOR
+
+Xiong Changnian  C<< <xiong@cpan.org> >>
+
+=head1 LICENSE
+
+Copyright (C) 2013 Xiong Changnian C<< <xiong@cpan.org> >>
+
+This library and its contents are released under Artistic License 2.0:
+
+L<http://www.opensource.org/licenses/artistic-license-2.0.php>
+
+=begin fool_pod_coverage
+
+No, I'm not just lazy. I think it's counterproductive to give each accessor 
+its very own section. Sorry if you disagree. 
+
+=head2 put_attr
+
+=head2 get_attr
+
+=cut
+
