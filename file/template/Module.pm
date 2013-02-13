05c586ba09cdc7c3391c84e7ebf6cc7d1e5f32a6
Xiong Changnian <xiong@cpan.org>
2013-02-12 18:57:14 -0800

    Create a shitty Module.pm template.
    Signed-off-by: Xiong Changnian <xiong@cpan.org>

---
+++ file/template/Module.pm
@@ -0,0 +1,37 @@
+package Devel::Toolbox;
+# Choose minimum perl interpreter version; delete the rest.
+# Do you want to enforce the bugfix level?
+#~ use 5.008008;   # 5.8.8     # 2006  # oldest sane version
+#~ use 5.008009;   # 5.8.9     # 2008  # latest 5.8
+#~ use 5.010001;   # 5.10.1    # 2009  # say, state, switch
+#~ use 5.012003;   # 5.12.5    # 2011  # yada
+#~ use 5.014002;   # 5.14.3    # 2012  # pop $arrayref, copy s///r
+#~ use 5.016002;   # 5.16.2    # 2012  # 
+use strict;
+use warnings;
+
+# Core modules
+
+# CPAN modules
+
+# Alternate uses
+#~ use Devel::Comments '###', ({ -file => 'debug.log' });                   #~
+
+## use
+#============================================================================#
+
+# Pseudo-globals
+
+## pseudo-globals
+#----------------------------------------------------------------------------#
+
+
+
+## END MODULE
+1;
+#============================================================================#
+__END__
+
+
+
+
