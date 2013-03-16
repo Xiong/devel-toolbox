970cdf8bd7819101c2839e98e700cc4ec9b43f63
Xiong Changnian <xiong@cpan.org>
2013-03-15 20:26:27 -0700

    Create util/sync.pl
    - Slay the Hydra!
    
    Signed-off-by: Xiong Changnian <xiong@cpan.org>

---
+++ util/sync.pl
@@ -0,0 +1,35 @@
+#!/usr/bin/env perl
+#       sync.pl
+#       = Copyright 2013 Xiong Changnian <xiong@cpan.org> =
+#       = Free Software = Artistic License 2.0 = NO WARRANTY =
+
+use 5.016002;   # 5.16.2    # 2012  # __SUB__
+use strict;
+use warnings;
+
+# Core modules
+use lib 'lib';
+
+# CPAN modules
+use Error::Base;                # Simple structured errors with full backtrace
+
+# Alternate uses
+use Devel::Comments '###';                                               #~
+#~ use Devel::Comments '###', ({ -file => 'debug.log' });                   #~
+
+## use
+#============================================================================#
+### $0 Running...
+
+my @watch_dirs  = qw|
+    ~/.config/$project
+    ./.config/$project
+    ~/.config/$project
+|;
+
+
+
+### $0 Done...
+exit 0;
+#============================================================================#
+__END__     
