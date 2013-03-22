ca27aff7959375ed840428c97c927799fa5bd737
Xiong Changnian <xiong@cpan.org>
2013-03-22 05:35:29 -0700

    Some small dinkery.
    Signed-off-by: Xiong Changnian <xiong@cpan.org>

--- lib/Devel/Toolbox/Core/Prove.pm
+++ lib/Devel/Toolbox/Core/Prove.pm
@@ -1,12 +1,5 @@
 package Devel::Toolbox::Core::Prove;
-# Choose minimum perl interpreter version; delete the rest.
-# Do you want to enforce the bugfix level?
-#~ use 5.008008;   # 5.8.8     # 2006  # oldest sane version
-#~ use 5.008009;   # 5.8.9     # 2008  # latest 5.8
-#~ use 5.010001;   # 5.10.1    # 2009  # say, state, switch
-#~ use 5.012003;   # 5.12.5    # 2011  # yada
-#~ use 5.014002;   # 5.14.3    # 2012  # pop $arrayref, copy s///r
-#~ use 5.016002;   # 5.16.2    # 2012  # __SUB__
+use 5.016002;   # 5.16.2    # 2012  # __SUB__
 use strict;
 use warnings;
 use version; our $VERSION = qv('v0.0.0');
@@ -35,7 +28,7 @@ __END__
 
 =head1 NAME
 
-Devel::Toolbox::Core::Prove - .................. 44 chars in PAUSE upload!
+Devel::Toolbox::Core::Prove - Test framework: high productivity, correctness
 
 =head1 VERSION
 
@@ -49,9 +42,9 @@ This document describes Devel::Toolbox::Core::Prove version v0.0.0
 
 =over
 
-I<Anyone can tell the truth, > 
-I<but only very few of us can make epigrams.> 
--- W. Somerset Maugham
+I<Doubt is useful, it keeps faith a living thing. After all, >
+I<you cannot know the strength of your faith until it has been tested. > 
+-- Pi Patel
 
 =back
 
