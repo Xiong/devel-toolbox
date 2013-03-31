2b867678360ddfcef3cb92ec2c8c6a410758e44d
Xiong Changnian <xiong@cpan.org>
2013-03-31 05:08:40 -0700

    vault() is a success... in play script.
    Signed-off-by: Xiong Changnian <xiong@cpan.org>

--- play/vault.pl
+++ play/vault.pl
@@ -18,24 +18,78 @@ use Devel::Comments '###', ({ -file => 'debug.log' });                   #~
 ## use
 #============================================================================#
 use Test::More;
+sub vault (&$);      # forward
+my $must_fail       = 1;
 
-my $builder = Test::More->builder;
-### $builder
-
-$builder->output(*STDERR);
-
-my $rv  = trap{
+my $rv  = vault {
     pass('I passed wrongly.');      # should fail
     fail('I failed correctly.');    # should pass
     
-};
+} $must_fail;
+
+### $rv
 
 
-pass('The kids are alright.');      # should pass anyway
-
-done_testing;
+#~ pass('The kids are alright.');      # should pass anyway
+#~ done_testing;
 exit(0);
 #----------------------------------------------------------------------------#
+
+sub vault (&$) {
+    my $coderef     = shift;
+    my $must_fail   = shift;
+    my $vault       = {};
+    my $stdout      ;
+    my @stdout      ;
+    my $stderr      ;
+    my $todo        ;
+    my $plan        = '1..';    # fill in with count
+    my $count       = 0;
+    
+    my $builder = Test::More->builder;
+    ### BEFORE DIDDLE
+    ### $builder
+    
+    $builder->        output(\$stdout);
+    $builder->failure_output(\$stderr);
+    $builder->   todo_output(\$todo  );
+    ### AFTER DIDDLE
+    ### $builder
+    
+    $vault->{return}    = &$coderef;
+    
+    say q{};
+    @stdout         = split /\n/, $stdout;
+    $count          = scalar @stdout;
+    $plan .= $count;
+    ### $must_fail
+    ### @stdout
+    ### $count
+    ### $plan
+    
+    if ($must_fail) {
+        for (@stdout) {
+            s/^not ok/Xok/;
+            s/^ok/not ok/;
+            s/^Xok/ok/;
+        };
+        ### AFTER MUST FAIL
+        ### @stdout
+        $stdout     = join qq{\n}, @stdout;
+    }
+    else {
+        $stdout     = join qq{\n}, @stdout;
+        print STDERR $stderr;
+    };
+    say STDOUT $stdout;
+    say STDOUT $plan;
+    
+    $vault->{stdout}    = $stdout;
+    $vault->{stderr}    = $stderr;
+    $vault->{todo}      = $todo;
+    
+    return $vault;
+};
 
 
 #============================================================================#
