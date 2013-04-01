122e3d2fa3e9ef3da21127d8144439cfa5de77d0
Xiong Changnian <xiong@cpan.org>
2013-04-01 16:57:03 -0700

    No, it's not spurious and we're not doing it.
    Signed-off-by: Xiong Changnian <xiong@cpan.org>

---
+++ play/small-test.pl
@@ -0,0 +1,30 @@
+#!/usr/bin/env perl
+#       play-2013.pl
+#       = Copyright 2013 Xiong Changnian <xiong@cpan.org> =
+#       = Free Software = Artistic License 2.0 = NO WARRANTY =
+
+use 5.016002;   # 5.16.2    # 2012  # __SUB__
+use strict;
+use warnings;
+
+#~ use lib qw| lib |;
+#~ use Error::Base;
+#~ use Perl6::Form;
+#~ use Test::More;
+#~ use Test::Trap;
+
+#~ use Devel::Comments '###';
+#~ use Devel::Comments '###', ({ -file => 'debug.log' });                   #~
+
+## use
+#============================================================================#
+say '1..1';
+say 'ok 1 Just a test.';
+
+
+exit;
+#----------------------------------------------------------------------------#
+
+
+#============================================================================#
+__END__     
