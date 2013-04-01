d7d0876f2de4b0e77ec9c6d6387e5e4b25b0f624
Xiong Changnian <xiong@cpan.org>
2013-04-01 16:48:10 -0700

    Spurious 'ok'?
    Signed-off-by: Xiong Changnian <xiong@cpan.org>

--- play/vault.pl
+++ play/vault.pl
@@ -8,86 +8,134 @@ use strict;
 use warnings;
 
 use lib qw| lib |;
-use Error::Base;
-use Perl6::Form;
-use Test::Trap;
+use Error::Base;                # Simple structured errors with full backtrace
+use Perl6::Form;                # Flexibly formatted output
+use Test::Trap;                 # Trap exit codes, exceptions, output, etc.
+use Clone                       # Recursively copy Perl datatypes
+    qw| clone |;
+use Capture::Tiny               # Capture STDOUT and STDERR
+    qw| :all |;
 
 #~ use Devel::Comments '###';
 use Devel::Comments '###', ({ -file => 'debug.log' });                   #~
 
 ## use
 #============================================================================#
+#~ use Test::More tests => 2;
 use Test::More;
 sub vault (&$);      # forward
 my $must_fail       = 1;
 
+pass('First check always passes.');
+
+# Checker vault, not case execution trap.
+# Rationally, one check at a time, please.
+# In this play script, should always be [not] ok 2.
 my $rv  = vault {
-    pass('I passed wrongly.');      # should fail
-    fail('I failed correctly.');    # should pass
+#~     pass('Second check passing');       # Test::More::pass()
+#~     fail('Second check failing');       # Test::More::fail()
+#~     say 'ok 2 Second check';            # talking to STDOUT directly
     
 } $must_fail;
 
 ### $rv
 
+my $builder = Test::More->builder;
+### BEFORE THIRD CHECK
+### $builder
 
-#~ pass('The kids are alright.');      # should pass anyway
-#~ done_testing;
-exit(0);
+
+pass('Third check always passes.');
+### AFTER THIRD CHECK
+### $builder
+
+done_testing;
+#~ say STDOUT '===';
+### AFTER DONE_TESTING
+### $builder
+
+#~ exit(0);
 #----------------------------------------------------------------------------#
 
 sub vault (&$) {
     my $coderef     = shift;
     my $must_fail   = shift;
     my $vault       = {};
+    my $bldout      ;
+    my @bldout      ;
+    my $blderr      ;
     my $stdout      ;
     my @stdout      ;
     my $stderr      ;
     my $todo        ;
     my $plan        = '1..';    # fill in with count
     my $count       = 0;
+    my $mario       ;           # clone of $builder
     
+    
+    # Clone/freeze Test::Builder.
     my $builder = Test::More->builder;
-    ### BEFORE DIDDLE
+    $mario          = clone($builder);
+    
+    # Misdirect Test::Builder output.
+    ### BEFORE MISDIRECT
+    ### $builder
+    ### $mario
+#~     $builder->        output(\$bldout);
+#~     $builder->failure_output(\$blderr);
+#~     $builder->   todo_output(\$todo  );
+    ### AFTER MISDIRECT
+    ### $builder
+    ### $mario
+    
+    
+    # Actual execution of the checker...
+    #   inside Capture::Tiny::capture{}
+    (                           # returns positional results; don't alter
+        $vault->{stdout},
+        $vault->{stderr},
+        $vault->{return},
+    ) 
+        = capture { &$coderef };
+    
+    ### AFTER CHECK
+    ### $builder
+    ### $mario
+    
+    
+#~     if ($must_fail) {
+#~         for (@stdout) {
+#~             s/^not ok/Xok/;
+#~             s/^ok/not ok/;
+#~             s/^Xok/ok/;
+#~         };
+#~         ### AFTER MUST FAIL
+#~         ### @stdout
+#~         $stdout     = join qq{\n}, @stdout;
+#~         $stderr     = '#   Check was declared to fail.';
+#~     }
+#~     else {
+#~         $stdout     = join qq{\n}, @stdout;
+#~     };
+    
+    
+    # Restore Test::Builder.
+#~     $Test::Builder::Test    = $mario;   # fuck with actual package variable
+#~     $builder->reset_outputs;
+    ### AFTER RESTORE
     ### $builder
     
-    $builder->        output(\$stdout);
-    $builder->failure_output(\$stderr);
-    $builder->   todo_output(\$todo  );
-    ### AFTER DIDDLE
-    ### $builder
+    # Do the fake check to stand in for the real check we just hid.
+#~     pass('Fake second check.');
     
-    $vault->{return}    = &$coderef;
+#~     say STDOUT $stdout;
+#~     say STDERR $stderr;
+#~     say STDOUT $plan;
     
-    say q{};
-    @stdout         = split /\n/, $stdout;
-    $count          = scalar @stdout;
-    $plan .= $count;
-    ### $must_fail
-    ### @stdout
-    ### $count
-    ### $plan
-    
-    if ($must_fail) {
-        for (@stdout) {
-            s/^not ok/Xok/;
-            s/^ok/not ok/;
-            s/^Xok/ok/;
-        };
-        ### AFTER MUST FAIL
-        ### @stdout
-        $stdout     = join qq{\n}, @stdout;
-        $stderr     = '#   Check was declared to fail.';
-    }
-    else {
-        $stdout     = join qq{\n}, @stdout;
-    };
-    say STDOUT $stdout;
-    say STDERR $stderr;
-    say STDOUT $plan;
-    
-    $vault->{stdout}    = $stdout;
-    $vault->{stderr}    = $stderr;
-    $vault->{todo}      = $todo;
+#~     # Store for later amusement. 
+#~     $vault->{stdout}    = $stdout;
+#~     $vault->{stderr}    = $stderr;
+#~     $vault->{todo}      = $todo;
     
     return $vault;
 };
